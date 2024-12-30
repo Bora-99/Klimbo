1ee6a1d54af8401f9cb5dbdd765bddc7b2d69daf
Bora-99 <selenicabora99@gmail.com>
2024-12-30 18:06:58 +0100

    init project & create routing

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LogInForm from "./components/LogIn/LogInForm";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Settings from "./pages/Settings";
+import Layout from "./components/LayOut/Layout";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/logIn" element={<LogInForm />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/settings" element={<Settings />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
